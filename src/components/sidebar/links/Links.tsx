ea4243d121e23ccc78bba0a15623d437bbd1252a
Ayush Singh <asayushsingh80@gmail.com>
2023-11-12 03:27:30 +0530

    implemented sideBar

--- src/components/sidebar/links/Links.tsx
+++ src/components/sidebar/links/Links.tsx
@@ -1,3 +1,30 @@
+import { Variants, motion } from "framer-motion";
+
+const variants: Variants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.01,
+      staggerDirection: -1,
+    },
+  },
+};
+
+const itemVariants: Variants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+};
+
 export default function Links() {
   const items: string[] = [
     "Homepage",
@@ -7,12 +34,18 @@ export default function Links() {
     "Contacts",
   ];
   return (
-    <div className="links">
+    <motion.div className="links" variants={variants}>
       {items.map((item) => (
-        <a href={`#${item}`} key={item}>
+        <motion.a
+          href={`#${item}`}
+          key={item}
+          variants={itemVariants}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+        >
           {item}
-        </a>
+        </motion.a>
       ))}
-    </div>
+    </motion.div>
   );
 }
