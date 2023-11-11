3f8c4945afbed78d998cba0a19d491b1eaf8c6c3
Ayush Singh <asayushsingh80@gmail.com>
2023-11-12 01:10:00 +0530

    basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./app.scss";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>hello</div>
-    </>
+    <div>
+      <section>Intro</section>
+      <section>parallax</section>
+      <section>services</section>
+      <section>Portfolio</section>
+      <section>Portfolio</section>
+      <section>Portfolio</section>
+    </div>
   );
 }
 
