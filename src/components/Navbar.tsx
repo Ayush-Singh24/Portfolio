afe2f0d159fa34159c935916392e7607061572d1
Ayush Singh <asayushsingh80@gmail.com>
2023-11-12 01:48:46 +0530

    navbar layout changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,15 +5,17 @@ export default function Navbar() {
     <div className="navbar">
       <div className="wrapper">
         <span>Ayush Singh</span>
-        <a href="https://github.com/Ayush-Singh24">
-          <img src="/images/github.png" alt="github" />
-        </a>
-        <a href="https://www.linkedin.com/in/ayush-singh-639112275/">
-          <img src="/images/linkedin.png" alt="linkedin" />
-        </a>
-        <a href="https://www.instagram.com/ayush.singh.24/">
-          <img src="/images/instagram.png" alt="instagram" />
-        </a>
+        <div className="socials">
+          <a href="https://github.com/Ayush-Singh24">
+            <img src="/images/github.png" alt="github" />
+          </a>
+          <a href="https://www.linkedin.com/in/ayush-singh-639112275/">
+            <img src="/images/linkedin.png" alt="linkedin" />
+          </a>
+          <a href="https://www.instagram.com/ayush.singh.24/">
+            <img src="/images/instagram.png" alt="instagram" />
+          </a>
+        </div>
       </div>
     </div>
   );
