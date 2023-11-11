61e79afb0712dbe2ca6c8dc8ef965f2950b99896
Ayush Singh <asayushsingh80@gmail.com>
2023-11-12 03:01:56 +0530

    implemented toggleButton

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,14 +1,36 @@
 import Links from "./links/Links";
-import "./sidebar.sccs";
+import { Variants, motion } from "framer-motion";
+import "./sidebar.scss";
 import ToggleButton from "./toggleButton/ToggleButton";
+import { useState } from "react";
 
 export default function Sidebar() {
+  const [open, setOpen] = useState<Boolean>(false);
+
+  const variants: Variants = {
+    open: {
+      clipPath: "circle(1200px at 50px 50px)",
+      transition: {
+        type: "spring",
+        stiffness: 20,
+      },
+    },
+    closed: {
+      clipPath: "circle(30px at 50px 50px)",
+      transition: {
+        delay: 0.5,
+        type: "spring",
+        stiffness: 400,
+        damping: 40,
+      },
+    },
+  };
   return (
-    <div className="sidebar">
-      <div className="bg">
+    <motion.div className="sidebar" animate={open ? "open" : "closed"}>
+      <motion.div className="bg" variants={variants}>
         <Links />
-      </div>
-      <ToggleButton />
-    </div>
+      </motion.div>
+      <ToggleButton setOpen={setOpen} />
+    </motion.div>
   );
 }
--- src/components/sidebar/toggleButton/ToggleButton.tsx
+++ src/components/sidebar/toggleButton/ToggleButton.tsx
@@ -1,3 +1,11 @@
-export default function ToggleButton() {
-  return <button>toggle</button>;
+import React, { Dispatch, SetStateAction } from "react";
+
+export default function ToggleButton({
+  setOpen,
+}: {
+  setOpen: Dispatch<SetStateAction<Boolean>>;
+}) {
+  return (
+    <button onClick={() => setOpen((prev: Boolean) => !prev)}>toggle</button>
+  );
 }
