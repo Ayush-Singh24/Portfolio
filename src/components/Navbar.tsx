96c933cbbf93872ea848f43afe81f55ca23307a6
Ayush Singh <asayushsingh80@gmail.com>
2023-11-12 01:37:26 +0530

    navbar layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,20 @@
 import "./navbar.scss";
 
 export default function Navbar() {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <span>Ayush Singh</span>
+        <a href="https://github.com/Ayush-Singh24">
+          <img src="/images/github.png" alt="github" />
+        </a>
+        <a href="https://www.linkedin.com/in/ayush-singh-639112275/">
+          <img src="/images/linkedin.png" alt="linkedin" />
+        </a>
+        <a href="https://www.instagram.com/ayush.singh.24/">
+          <img src="/images/instagram.png" alt="instagram" />
+        </a>
+      </div>
+    </div>
+  );
 }
