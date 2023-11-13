e2e0bc8e09ee839d62619e682fe58cba3e4e1068
Ayush Singh <asayushsingh80@gmail.com>
2023-11-13 16:11:33 +0530

    changes font size

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./app.scss";
+import { Home } from "./components/home/Home";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div>
       <section>
         <Navbar />
+        <Home />
       </section>
       <section>parallax</section>
       <section>services</section>
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,4 +1,10 @@
 import "./home.scss";
 export function Home() {
-  return <div>home</div>;
+  return (
+    <div className="home">
+      <div className="imageContainer">
+        <img src="/images/ayush.png" alt="profile" />
+      </div>
+    </div>
+  );
 }
