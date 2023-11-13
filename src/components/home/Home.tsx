2ef899678d12d85bbd512b2a262cc6aae4c2fc54
Ayush Singh <asayushsingh80@gmail.com>
2023-11-13 16:45:38 +0530

    basic home UI

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,8 +2,18 @@ import "./home.scss";
 export function Home() {
   return (
     <div className="home">
-      <div className="imageContainer">
-        <img src="/images/ayush.png" alt="profile" />
+      <div className="wrapper">
+        <div className="textContainer">
+          <h2>AYUSH SINGH</h2>
+          <h1>Web developer and UI designer</h1>
+          <div className="buttons">
+            <button>See the latest projects</button>
+            <button>Contact</button>
+          </div>
+        </div>
+        <div className="imageContainer">
+          <img src="/images/ayush.png" alt="profile" />
+        </div>
       </div>
     </div>
   );
