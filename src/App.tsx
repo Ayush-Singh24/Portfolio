24a9d8f432729df6f1618b2c3480ddc5a62875b6
Ayush Singh <asayushsingh80@gmail.com>
2023-11-14 14:00:42 +0530

    basic certificates layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./app.scss";
 import AboutMe from "./components/aboutMe/AboutMe";
+import Ceritficates from "./components/certificates/Ceritificates";
 import { Home } from "./components/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
@@ -18,9 +19,11 @@ function App() {
         <Parallax text="Ceritficates" color="dark" />
       </section>
       <section>
+        <Ceritficates />
+      </section>
+      <section>
         <Parallax text="Projects" color="light" />
       </section>
-      <section>Portfolio</section>
       <section>Portfolio</section>
     </div>
   );
---
+++ src/components/certificates/Ceritificates.tsx
@@ -0,0 +1,10 @@
+import "./certificates.scss";
+export default function Ceritficates() {
+  return (
+    <div className="certificates">
+      <div className="wrapper">
+        <div className="card">hello</div>
+      </div>
+    </div>
+  );
+}
