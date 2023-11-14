d13f55b0902b39b597b69ff991e005d3c20f3489
Ayush Singh <asayushsingh80@gmail.com>
2023-11-14 16:39:17 +0530

    removed placeholder card

--- src/components/certificates/Ceritificates.tsx
+++ src/components/certificates/Ceritificates.tsx
@@ -10,11 +10,13 @@ export default function Ceritficates() {
               title="NPTEL : Data Science For Engineers"
               description="This comprehensive course from NPTEL, the National Programme on Technology Enhanced Learning, provides a solid foundation in data science for engineers. It covers the essential concepts, tools, and techniques for extracting valuable insights from data."
               buttonTag="Download"
+              link="/docs/nptelds.pdf"
             />
             <Card
               title="SIH Internal Hackathon"
               description="In a display of exceptional problem-solving skills and innovative thinking, I, along with my team, emerged as the first-place winners for our problem-statement at the SIH Internal Hackathon."
               buttonTag="Download"
+              link="/docs/sih.pdf"
             />
             <Card
               title="TALENT HUNT - 2023 (Coding Contest)"
@@ -28,11 +30,11 @@ export default function Ceritficates() {
               description="I am proud to share that I was awarded second place in Project Presentation at the prestigious Talent Hunt 2023 Hackathon, organized by the DSBS department of SRM University. This hackathon brought together some of the brightest minds in technology to tackle real-world challenges and develop innovative solutions."
               buttonTag="Download"
             />
-            <Card
+            {/* <Card
               title="hello"
               description="font-size: medium;font-size: medium;font-size: medium;font-size: medium;font-size: medium;font-size: medium;font-size: medium;font-size: medium;"
               buttonTag="Download"
-            />
+            /> */}
             <Card
               title="NPTEL : Programming in Java"
               description="The NPTEL Course on Programmin in Java is a comprehensive course that covers the essential concepts and skills of Java programming, one of the most popular and widely used programming languages today."
